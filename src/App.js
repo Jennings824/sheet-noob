af8807eaf5d97cec5931827d854ebab32d79d215
Michael <MichaelJennings824@gmail.com>
2020-03-26 17:33:57 -0400

    added Intro page text and a ready button that creates a nicer initial experience

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useState  } from 'react';
 import './App.css';
 
 import NoteContainer from './NoteContainer/NoteContainer.js'
 
 function App() {
+  const [userReady, setUserReady] = useState(false) 
+
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -12,7 +16,16 @@ function App() {
         </p>
       </header>
 
-    <NoteContainer/>
+
+      <h1 className="Intro">Hello, folks! Welcome to this app i decided to make since I stink at remembering notes on sheet music. Here's to learning!</h1>
+      
+      { !userReady &&
+        <button className="Intro-button" onClick={() => setUserReady(true)}>Ready to Start Your Journey?</button>
+      }
+
+      {userReady &&
+        <NoteContainer/>
+      }
     </div>
   );
 }
