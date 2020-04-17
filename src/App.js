9763ff2c331d34bf9ede6d25572c6ccdfd2ce403
Michael <MichaelJennings824@gmail.com>
2020-04-16 23:05:54 -0400

    refactor/got some images in

--- src/App.js
+++ src/App.js
@@ -1,31 +1,27 @@
-import React, { useState  } from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
-import NoteContainer from './NoteContainer/NoteContainer.js'
+import NoteContainer from "./NoteContainer/NoteContainer.js";
 
 function App() {
-  const [userReady, setUserReady] = useState(false) 
-
-
-
+  const [userReady, setUserReady] = useState(false);
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Sheet n00b
-        </p>
+        <p>Sheet n00b</p>
       </header>
+      <h1 className="Intro">
+        Hello, folks! This app is aimed at helping you 
+        remember notes on sheet music.
+      </h1>
 
+      {!userReady && (
+        <button className="Intro-button" onClick={() => setUserReady(true)}>
+          Ready to Start?
+        </button>
+      )}
 
-      <h1 className="Intro">Hello, folks! Welcome to this app i decided to make since I stink at remembering notes on sheet music. Here's to learning!</h1>
-      
-      { !userReady &&
-        <button className="Intro-button" onClick={() => setUserReady(true)}>Ready to Start Your Journey?</button>
-      }
-
-      {userReady &&
-        <NoteContainer/>
-      }
+      {userReady && <NoteContainer />}
     </div>
   );
 }
